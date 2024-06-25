8cfc0710925909f76cba40b02493b4c8e8c021d0
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-06-25 15:03:37 +0000

    enable install with --yes

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Add mise CLI and activate it
 curl https://mise.run | sh
 echo 'eval "$(mise activate bash)"' >> ~/.bashrc
-# source ~/.bashrc
 
 # Make mise install the tools from .mise.toml
-# mise install
+mise install --yes
