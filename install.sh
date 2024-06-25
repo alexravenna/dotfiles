280df96a958da32840cfc19b90725b76801ea5ba
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-06-25 14:55:23 +0000

    try activating mise with shims

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Add mise CLI and activate it
 curl https://mise.run | sh
-echo 'eval "$(mise activate bash)"' >> ~/.bashrc
+echo 'eval "$(mise activate --shims)"' >> ~/.bashrc
 source ~/.bashrc
 
 # Make mise install the tools from .mise.toml
